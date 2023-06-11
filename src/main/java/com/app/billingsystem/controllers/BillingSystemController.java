bd6723564fd67ac15eff9b3c0ff44edcaca2a900
Priyaranjan <priyaranjanmantri@gmail.com>
2023-06-11 12:38:14 +0530

    added: architecture

---
+++ src/main/java/com/app/billingsystem/controllers/BillingSystemController.java
@@ -0,0 +1,10 @@
+package com.app.billingsystem.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/billing-system")
+//@Tag(name = "Billings System")
+public class BillingSystemController {
+}
---
+++ src/main/java/com/app/billingsystem/controllers/IBillingSystemController.java
@@ -0,0 +1,4 @@
+package com.app.billingsystem.controllers;
+
+public interface IBillingSystemController {
+}
