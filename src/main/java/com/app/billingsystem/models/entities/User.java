a242e3706f5a778047ea45c1a55fb115efa3acd6
debadatta <dparida734@gmail.com>
2023-06-12 20:40:19 +0530

    bump : phone number removed

--- src/main/java/com/app/billingsystem/models/entities/User.java
+++ src/main/java/com/app/billingsystem/models/entities/User.java
@@ -18,8 +18,6 @@ public class User {
     @Column(nullable = false, unique = true)
     private String email;
     @Column(nullable = false, unique = true)
-    private int phoneNumber;
-    @Column(nullable = false, unique = true)
     private String password;
 
 
