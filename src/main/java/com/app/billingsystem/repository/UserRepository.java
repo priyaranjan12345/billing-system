c28b26fea7d9b993e234137c86bc92b5e0df04d1
Priyaranjan <priyaranjanmantri@gmail.com>
2023-06-12 21:34:23 +0530

    added: jwt auth repo

---
+++ src/main/java/com/app/billingsystem/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.app.billingsystem.repository;
+
+import com.app.billingsystem.models.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Boolean existsByEmail(String email);
+    Optional<User> findByEmail(String email);
+}
