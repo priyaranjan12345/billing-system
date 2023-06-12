979694456dc75ce2083f02ac4731cb9eb198d452
debadatta <dparida734@gmail.com>
2023-06-12 20:37:40 +0530

    add : user and role entities

--- src/main/java/com/app/billingsystem/models/entities/User.java
+++ src/main/java/com/app/billingsystem/models/entities/User.java
@@ -1,8 +1,26 @@
 package com.app.billingsystem.models.entities;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
 public class User {
-    // id auto gen
-    // phoneNumber
-    // email address
-    // password
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false, unique = true)
+    private String email;
+    @Column(nullable = false, unique = true)
+    private int phoneNumber;
+    @Column(nullable = false, unique = true)
+    private String password;
+
+
 }
