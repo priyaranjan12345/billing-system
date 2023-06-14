08eb9bf169db4fced6b2a23a9b52bf5ca04bfe5f
debadatta <dparida734@gmail.com>
2023-06-14 21:16:33 +0530

    add: getter and setter for login

---
+++ src/main/java/com/app/billingsystem/models/dtos/LoginDto.java
@@ -0,0 +1,16 @@
+package com.app.billingsystem.models.dtos;
+import lombok.*;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+public class LoginDto {
+
+    private String email;
+
+    private String password;
+
+}
