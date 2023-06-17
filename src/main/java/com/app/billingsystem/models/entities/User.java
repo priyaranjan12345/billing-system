5023d658c0febac85cf9f5076ae2235e6d267475
Priyaranjan <priyaranjanmantri@gmail.com>
2023-06-17 21:50:24 +0530

    bump: added builder annotation

--- src/main/java/com/app/billingsystem/models/entities/User.java
+++ src/main/java/com/app/billingsystem/models/entities/User.java
@@ -1,12 +1,10 @@
 package com.app.billingsystem.models.entities;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.util.Set;
 
+@Builder
 @Setter
 @Getter
 @NoArgsConstructor
