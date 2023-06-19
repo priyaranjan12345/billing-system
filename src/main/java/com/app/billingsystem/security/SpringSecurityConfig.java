722fcb78bd216b4e5c1c6554872c599ebc5ebaba
Priyaranjan <priyaranjanmantri@gmail.com>
2023-06-19 21:47:16 +0530

    bump: config secure url

--- src/main/java/com/app/billingsystem/security/SpringSecurityConfig.java
+++ src/main/java/com/app/billingsystem/security/SpringSecurityConfig.java
@@ -6,13 +6,17 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
 @Configuration
+@EnableWebSecurity
 @AllArgsConstructor
 public class SpringSecurityConfig {
     private UserDetailsService userDetailsService;
@@ -25,8 +29,16 @@ public class SpringSecurityConfig {
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         // Disable CSRF protection
-        http.csrf(AbstractHttpConfigurer::disable).authorizeHttpRequests(authorize -> authorize.requestMatchers("/api/auth/**").permitAll()
-                .anyRequest().authenticated());
+        http.csrf(AbstractHttpConfigurer::disable).authorizeHttpRequests(
+                        authorize -> authorize
+                                .requestMatchers("/favicon.ico").permitAll()
+                                .requestMatchers("/api/auth/secured").permitAll()
+                                .requestMatchers("/api/auth/**")
+                                .permitAll()
+                                .anyRequest()
+                                .authenticated()
+                ).oauth2Login(withDefaults())
+                .formLogin(withDefaults());
         return http.build();
     }
 
