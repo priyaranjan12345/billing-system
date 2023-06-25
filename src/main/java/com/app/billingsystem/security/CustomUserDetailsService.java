9fbec493560986fdb01526dfbbca20db71e5427f
Priyaranjan <priyaranjanmantri@gmail.com>
2023-06-25 19:20:55 +0530

    bump: code formatted

--- src/main/java/com/app/billingsystem/security/CustomUserDetailsService.java
+++ src/main/java/com/app/billingsystem/security/CustomUserDetailsService.java
@@ -20,7 +20,6 @@ public class CustomUserDetailsService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-
         User user = userRepository.findByEmail(email)
                 .orElseThrow(() -> new UsernameNotFoundException("User not exists by Username or Email"));
 
