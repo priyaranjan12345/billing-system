c6a65fcc54b5543f9355feeec8ade2a2e7a98bc8
Gnyan-Coder <gnyanranjanbehera@gmail.com>
2023-09-17 16:11:45 +0530

    added delete and get all item

--- src/main/java/com/app/billingsystem/controllers/UserController.java
+++ src/main/java/com/app/billingsystem/controllers/UserController.java
@@ -1,4 +1,5 @@
 package com.app.billingsystem.controllers;
+import com.app.billingsystem.helper.PageableResponse;
 import com.app.billingsystem.models.dtos.CartItemsDto;
 
 import com.app.billingsystem.models.dtos.ItemRequest;
@@ -6,6 +7,7 @@ import com.app.billingsystem.models.dtos.ItemResponse;
 import com.app.billingsystem.service.ItemService;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -36,11 +38,31 @@ public class UserController {
 
     }
 
+
     @PutMapping(value = "/update-item-details/{id}")
     public ResponseEntity<ItemResponse> updateItemDetails(@PathVariable("id") Long id, @Valid @ModelAttribute ItemRequest itemRequest) throws Exception {
         ItemResponse itemResponse = itemService.updateItem(id, itemRequest);
         return new ResponseEntity<>(itemResponse, HttpStatus.OK);
     }
+
+    @GetMapping(value = "/get-all-item")
+    public  ResponseEntity<PageableResponse<ItemResponse>> getAllItem(
+            @RequestParam(value = "pageNumber",defaultValue = "0",required = false) int pageNumber,
+            @RequestParam(value = "pageSize",defaultValue = "10",required = false) int pageSize,
+            @RequestParam(value = "sortBy",defaultValue = "name",required = false) String sortBy,
+            @RequestParam(value = "sortDir",defaultValue = "asc",required = false) String sortDir){
+        return  new ResponseEntity<>(itemService.getAllItem(pageNumber,pageSize,sortBy,sortDir),HttpStatus.OK);
+    }
+
+
+
+
+    @DeleteMapping(value ="/delete-item/{id}")
+    public ResponseEntity<String> deleteItem(@PathVariable Long id) {
+        itemService.deleteItem(id);
+        return new ResponseEntity<>("Item deleted!!",HttpStatus.OK);
+    }
+
 
 
     @PostMapping(value = "/save-order-generate-bill")
