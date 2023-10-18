8950da169bdbba63c5bb4b193bbdbe9123ba8eeb
Priyaranjan <priyaranjanmantri@gmail.com>
2023-10-18 20:58:06 +0530

    added: subscription controller

---
+++ src/main/java/com/app/billingsystem/controllers/UserPaymentController.java
@@ -0,0 +1,16 @@
+package com.app.billingsystem.controllers;
+
+public class UserPaymentController {
+    // save user subscription details
+    // get user subscription details
+    /**
+     * {
+     *     planType: weekly. yearly, monthly,
+     *     paymentId: pid,
+     *     signature: sign,
+     *     orderId: oid,
+     *
+     * }
+     *
+     * */
+}
